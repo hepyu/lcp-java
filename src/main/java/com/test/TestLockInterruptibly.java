70a4b189804fb4b688bcf0b7cf08e1294a8c6306
hepengyuan <hepengyuan@xunlei.com>
2017-04-21 15:56:56 +0800

    update

--- src/main/java/com/test/TestLockInterruptibly.java
+++ src/main/java/com/test/TestLockInterruptibly.java
@@ -8,19 +8,31 @@ public class TestLockInterruptibly {
 	Lock lock = new ReentrantLock();
 
 	public static void main(String[] args) {
-		TestLockInterruptibly test = new TestLockInterruptibly();
-
-		MyThread thread0 = new MyThread(test);
-		MyThread thread1 = new MyThread(test);
-
-		thread0.start();
-		thread1.start();
-
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		
+		long allCommentNum = 100;
+		long playNum = 500;
+		long praiseNum = 20;
+		
+		
+		double rate1 = (allCommentNum*1.0/playNum);
+		double rate2 = (praiseNum*1.0/playNum);
+		
+		System.out.println("rate1:"+rate1);
+		System.out.println("rate2:"+rate2);
+		
+//		TestLockInterruptibly test = new TestLockInterruptibly();
+//
+//		MyThread thread0 = new MyThread(test);
+//		MyThread thread1 = new MyThread(test);
+//
+//		thread0.start();
+//		thread1.start();
+//
+//		try {
+//			Thread.sleep(2000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
 
 		// thread1.interrupt();
 	}
@@ -28,7 +40,7 @@ public class TestLockInterruptibly {
 	public void insert(Thread thread) throws InterruptedException {
 		lock.lockInterruptibly();
 		try {
-			System.out.println(thread.getName() + "得到了锁");
+			System.out.println(thread.getName() + "寰楀埌浜嗛攣");
 			long startTime = System.currentTimeMillis();
 			for (;;) {
 				if (System.currentTimeMillis() - startTime >= 5000) {
@@ -38,9 +50,9 @@ public class TestLockInterruptibly {
 		} catch (Exception e) {
 			// TODO
 		} finally {
-			System.out.println(Thread.currentThread().getName() + "执行finally");
+			System.out.println(Thread.currentThread().getName() + "鎵цfinally");
 			lock.unlock();
-			System.out.println(thread.getName() + "释放了锁");
+			System.out.println(thread.getName() + "閲婃斁浜嗛攣");
 		}
 	}
 
@@ -58,7 +70,7 @@ class MyThread extends Thread {
 		try {
 			test.insert(Thread.currentThread());
 		} catch (InterruptedException e) {
-			System.out.println(Thread.currentThread().getName() + "被中断");
+			System.out.println(Thread.currentThread().getName() + "琚腑鏂�");
 		}
 	}
 }
